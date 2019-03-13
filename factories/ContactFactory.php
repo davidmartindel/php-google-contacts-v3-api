e4362a1d046474326a24cf8bbdc828da5e32b831
J. David Martin Delgado <martinda@gmail.com>
2019-03-13 11:40:57 +0100

    simple test

--- factories/ContactFactory.php
+++ factories/ContactFactory.php
@@ -7,6 +7,74 @@ use rapidweb\googlecontacts\objects\Contact;
 
 abstract class ContactFactory
 {
+        public static function getFirstContact($customConfig = NULL)
+    {
+        $client = GoogleHelper::getClient($customConfig);
+
+        $req = new \Google_Http_Request('https://www.google.com/m8/feeds/contacts/default/full?max-results=1&updated-min=2007-03-16T00:00:00');
+
+        $val = $client->getAuth()->authenticatedRequest($req);
+
+        $response = $val->getResponseBody();
+
+        $xmlContacts = simplexml_load_string($response);
+        $xmlContacts->registerXPathNamespace('gd', 'http://schemas.google.com/g/2005');
+
+        $contactsArray = array();
+
+        foreach ($xmlContacts->entry as $xmlContactsEntry) {
+            $contactDetails = array();
+
+            $contactDetails['id'] = (string) $xmlContactsEntry->id;
+            $contactDetails['name'] = (string) $xmlContactsEntry->title;
+            $contactDetails['content'] = (string) $xmlContactsEntry->content;
+
+            foreach ($xmlContactsEntry->children() as $key => $value) {
+                $attributes = $value->attributes();
+
+                if ($key == 'link') {
+                    if ($attributes['rel'] == 'edit') {
+                        $contactDetails['editURL'] = (string) $attributes['href'];
+                    } elseif ($attributes['rel'] == 'self') {
+                        $contactDetails['selfURL'] = (string) $attributes['href'];
+                    } elseif ($attributes['rel'] == 'http://schemas.google.com/contacts/2008/rel#edit-photo') {
+                        $contactDetails['photoURL'] = (string) $attributes['href'];
+                    }
+                }
+            }
+
+            $contactGDNodes = $xmlContactsEntry->children('http://schemas.google.com/g/2005');
+            foreach ($contactGDNodes as $key => $value) {
+                switch ($key) {
+                    case 'organization':
+                        $contactDetails[$key]['orgName'] = (string) $value->orgName;
+                        $contactDetails[$key]['orgTitle'] = (string) $value->orgTitle;
+                        break;
+                    case 'email':
+                        $attributes = $value->attributes();
+                        $emailadress = (string) $attributes['address'];
+                        $emailtype = substr(strstr($attributes['rel'], '#'), 1);
+                        $contactDetails[$key][] = ['type' => $emailtype, 'email' => $emailadress];
+                        break;
+                    case 'phoneNumber':
+                        $attributes = $value->attributes();
+                        //$uri = (string) $attributes['uri'];
+                        $type = substr(strstr($attributes['rel'], '#'), 1);
+                        //$e164 = substr(strstr($uri, ':'), 1);
+                        $contactDetails[$key][] = ['type' => $type, 'number' => $value->__toString()];
+                        break;
+                    default:
+                        $contactDetails[$key] = (string) $value;
+                        break;
+                }
+            }
+
+            $contactsArray[] = new Contact($contactDetails);
+        }
+
+        return $contactsArray;
+    }
+    
     public static function getAll($customConfig = NULL)
     {
         $client = GoogleHelper::getClient($customConfig);
